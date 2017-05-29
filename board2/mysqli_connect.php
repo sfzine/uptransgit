b9d1c86f6a2f2c3e385ce63f40c40bde34e3f469
sfzine <sfzine@naver.com>
2017-05-29 17:48:20 +0900

    Update mysqli_connect.php

--- board2/mysqli_connect.php
+++ board2/mysqli_connect.php
@@ -6,11 +6,12 @@
 // Set the database access information as constants:
 DEFINE ('DB_USER', 'navyism');
 DEFINE ('DB_PASSWORD', 'a1234');
-DEFINE ('DB_HOST', '172.30.150.173');
+DEFINE ('DB_HOST', '172.45.01.234');
 DEFINE ('DB_NAME', 'uptrans');
 
 // Make the connection:
 //$dbc = @mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
 
 $dbc = mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
